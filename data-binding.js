efb3d677f5f926a6c5b3a687deebf667f496f09e
Mathias Hansen <mathias.hansen783@gmail.com>
2015-05-27 13:22:47 +0200

    added repeat

--- data-binding.js
+++ data-binding.js
@@ -8,7 +8,7 @@ var Model;
         this.bind = {
             types: {
                 text: function (property, binder) {
-                    binder.innerHTML = that.getValue(property);
+                    binder.innerHTML = that.value.get(property);
                 },
                 value: function (property, binder) {
                     if (binder.nodeName === "INPUT") {
@@ -17,9 +17,9 @@ var Model;
                         if (binder.type === "checkbox")
                             event = "change";
 
-                        binder.value = that.getValue(property);
+                        binder.value = that.value.get(property);
                         binder.addEventListener(event, function () {
-                            that.setValue(property, this.value);
+                            that.value.set(property, this.value);
                         });
                     } else {
                         console.error("element bound with value type must be input");
@@ -41,10 +41,32 @@ var Model;
                         .replace(/\(/, "")
                         .replace(/\)/, "");
 
-                    binder.setAttribute(attr, that.getValue(property));
+                    binder.setAttribute(attr, that.value.get(property));
                 },
                 repeat: function (property, binder) {
+                    var array = vm[property],
+                        template = document.createElement("div");
 
+                    template.innerHTML = binder.innerHTML;
+
+                    binder.innerHTML = "";
+
+                    array.forEach(function (item) {
+                        var clone = template.cloneNode(),
+                            binders;
+
+                        clone.innerHTML = template.innerHTML;
+
+                        binders = clone.querySelectorAll("*[bind]");
+
+                        vm.$item = item;
+
+                        for (var i = 0; i < binders.length; i++) {
+                            that.bind.element(binders[i]);
+                        }
+
+                        binder.innerHTML += clone.innerHTML;
+                    });
                 }
             },
             element: function (binder) {
@@ -58,71 +80,76 @@ var Model;
                     var property = bindAttr[0],
                         type = bindAttr[1];
 
+                    console.log(property);
+
                     that.bind.types[type.match(/^\w+/)](property, binder, type);
                 });
             }
         };
-        this.getObject = function (property) {
-            var path = property.split("."),
-                value = vm[path[0]];
-
-            path.splice(0, 1);
-
-            while (path.length !== 0) {
-                value = value[path[0]];
-                path.splice(0, 1);
-            }
-
-            return value;
-        },
-        this.getFunction = function (property, root) {
-            var params = that.getParams(property),
-                values = [];
-
-            params.forEach(function (param) {
-                values.push(that.getValue(param));
-            });
-
-            return vm[root].apply(undefined, values);
-        },
-        this.getValue = function (property) {
-            var root = property.match(/^[\w.]+/)[0];
-
-            if (!vm[root])
-                return that.getObject(property);
-            else if (typeof vm[root] === "function")
-                return that.getFunction(property, root);
-            else
-                return vm[root];
-        };
-        this.setValue = function (property, value) {
-            if (vm[property] === undefined) {
+        this.value = {
+            object: function (property) {
                 var path = property.split("."),
-                    prop = vm[path[0]];
+                    value = vm[path[0]];
 
                 path.splice(0, 1);
 
-                while (path.length !== 1) {
-                    prop = prop[path[0]];
+                while (path.length !== 0) {
+                    value = value[path[0]];
                     path.splice(0, 1);
                 }
 
-                prop[path[0]] = value;
-            } else {
-                vm[property] = value;
-            }
-        };
-        this.getParams = function (value) {
-            var params = value.match(/\(.+?\)/);
+                return value;
+            },
+            function: function (property, root) {
+                var params = that.value.params(property),
+                    values = [];
+                if (params) {
+                    params.forEach(function (param) {
+                        values.push(that.value.get(param));
+                    });
+                }
 
-            if (params) {
-                params = params[0]
-                    .replace(/\(/, "")
-                    .replace(/\)/, "")
-                    .replace(/\s/g, "")
-                    .split(",");
+                return vm[root].apply(undefined, values);
+            },
+            params: function (value) {
+                var params = value.match(/\(.+?\)/);
 
-                return params;
+                if (params) {
+                    params = params[0]
+                        .replace(/\(/, "")
+                        .replace(/\)/, "")
+                        .replace(/\s/g, "")
+                        .split(",");
+
+                    return params;
+                }
+            },
+            get: function (property) {
+                var root = property.match(/^[$\w\.]+/)[0];
+
+                if (!vm[root])
+                    return that.value.object(property);
+                else if (typeof vm[root] === "function")
+                    return that.value.function(property, root);
+                else
+                    return vm[root];
+            },
+            set: function (property, value) {
+                if (vm[property] === undefined) {
+                    var path = property.split("."),
+                        prop = vm[path[0]];
+
+                    path.splice(0, 1);
+
+                    while (path.length !== 1) {
+                        prop = prop[path[0]];
+                        path.splice(0, 1);
+                    }
+
+                    prop[path[0]] = value;
+                } else {
+                    vm[property] = value;
+                }
             }
         };
         this.getUses = function (binders) {
@@ -138,7 +165,7 @@ var Model;
                         pos: [i]
                     };
 
-                    var params = that.getParams(value);
+                    var params = that.value.params(value);
 
                     if (params) {
                         params.forEach(function (param) {
@@ -164,22 +191,24 @@ var Model;
                 changes.forEach(function (change) {
                     var name = ((root) ? root : "") + change.name;
 
-                    if (uses[name].pos) {
-                        uses[name].pos.forEach(function (pos) {
-                            that.bind.element(binders[pos]);
-                        });
-                    }
-                    if (uses[name].func) {
-                        uses[name].func.forEach(function (func) {
-                            uses[func].pos.forEach(function (pos) {
+                    if (change.name !== "$item") {
+                        if (uses[name].pos) {
+                            uses[name].pos.forEach(function (pos) {
                                 that.bind.element(binders[pos]);
                             });
-                        });
+                        }
+                        if (uses[name].func) {
+                            uses[name].func.forEach(function (func) {
+                                uses[func].pos.forEach(function (pos) {
+                                    that.bind.element(binders[pos]);
+                                });
+                            });
+                        }
                     }
                 });
             });
             for (var prop in obj) {
-                if (typeof obj[prop] === "object")
+                if (typeof obj[prop] === "object" && prop !== "$item")
                     that.observe(obj[prop], uses, binders, ((root) ? root : "") + prop + ".");
             }
         };
@@ -198,7 +227,8 @@ var Model;
             that.observe(vm, uses, binders);
 
             for (var i = 0; i < binders.length; i++) {
-                that.bind.element(binders[i]);
+                if (binders[i].getAttribute("bind").match(/\$item/) === null)
+                    that.bind.element(binders[i]);
             }
         };
 
